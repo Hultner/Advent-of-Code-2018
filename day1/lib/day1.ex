0ee384a20306fd4a6266d5276cc99bfeabf4e9d3
Alexander Hultnér <ahultner@gmail.com>
2017-12-12 18:08:48 +0100

    Day 1 refactoring & reformatting

--- day1/lib/day1.ex
+++ day1/lib/day1.ex
@@ -5,19 +5,14 @@ defmodule Day1 do
     |> Enum.reduce(0, fn {x, _}, acc -> acc + x end)
   end
 
+  def puzzle_sum_half(list) do
+    Enum.split(list, round(Enum.count(list) / 2))
+    |> (fn {x, y} -> Enum.concat(y, x) end).()
+    |> Enum.zip(list)
+    |> Enum.filter(fn {x, y} -> x == y end)
+    |> Enum.reduce(0, fn {x, _}, acc -> acc + x end)
+  end
 
-    def solution_part_1(number),
-       do: Integer.digits(number) |> puzzle_sum
-           
-    def puzzle_sum_half(list) do
-       Enum.split(list, round(Enum.count(list)/2) )
-       |> (fn {x, y} -> Enum.concat(y,x) end).()
-       |> (Enum.zip list)
-       |> (Enum.filter fn {x, y} -> x == y end)
-       |> (Enum.reduce 0, fn {x, _}, acc -> acc+x end)
-    end
-
-    def solution_part_2(number),
-       do: Integer.digits(number) |> puzzle_sum_half
-           
+  def solution_part_1(number), do: Integer.digits(number) |> puzzle_sum
+  def solution_part_2(number), do: Integer.digits(number) |> puzzle_sum_half
 end
